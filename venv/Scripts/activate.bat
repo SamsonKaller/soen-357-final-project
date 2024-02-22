2ddeec90e9e41fe9cfc4491653d0ce67138aa6ea
SamsonKaller <samson.kaller@gmail.com>
2024-02-22 12:37:48 -0500

    update VENV packages to work with newer version of python (3.12)

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\samso\Desktop\example\venv
+set VIRTUAL_ENV=C:\Users\samso\Documents\Concordia\Semester08\SOEN357\project\soen-357-final-project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
